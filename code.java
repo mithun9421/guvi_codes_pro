7c3f5e6b9333688298c88ebde67715faad496f1d
mithun9421 <mithunmuralee94@gmail.com>
2016-09-12 08:57:17 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,25 @@
+package apple;
+import java.util.Scanner;
+public class Apple {
+public static void main(String[] args, int i) {
+      String str="hello";
+    for( i=0;i<=str.length();i++)
+    {
+      char e=str.charAt(i);
+        if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+        {
+        String h=str.substring(0, i);
+
+String g=str.substring(i+ i);
+str=h+"Z"+g;
+}}
+    
+    
+    
+}
+    System.out.println("The result is="+str);
+
+
+
+                
+    }}
